415318b47f6915899e0fa5c073bb2253008b1ea9
John Mburu <johnnkonge2020@gmail.com>
2023-09-25 12:54:06 +0300

    Find() trial

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const obj = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function superBowlWin(obj){
+    for(item in obj){
+        if(item.result === 'W'){
+            console.log(item)
+        }
+    }
+    
+};
+obj.find(superBowlWin)
